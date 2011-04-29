df75d61c94668dcd63323c2b2ee6a0f737f16017
Joe Leaver <joe@joeleaver.com>
2011-04-29 15:31:36 -0700

    Removed conditional devise_modules.include?(:validatable) Wasn't working.

--- lib/devise/orm/neo4j/hook.rb
+++ lib/devise/orm/neo4j/hook.rb
@@ -6,7 +6,7 @@ module Devise
           extend Schema
           
           # hack to get around Neo4j's requirement to index before uniqueness validation
-          index :email, :type => :exact if devise_modules.include?(:validatable)
+          index :email, :type => :exact # if devise_modules.include?(:validatable)
             
           yield
           devise_modules.each { |m| send(m) if respond_to?(m, true) }
